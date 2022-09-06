5737d1a9d56530f17106cee423fa5ed6c68fe6d9
Joshua Wootonn <josh@makeswift.com>
2022-09-06 09:59:17 -0500

    feat: add cart / header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
+import { CartProvider } from "lib/cart-context";
 import { ProductsContext } from "lib/products-context";
 import { ProductContext } from "lib/product-context";
 
 export default function App({ Component, pageProps }: any) {
   return (
-    <ProductsContext.Provider value={pageProps.products}>
-      <ProductContext.Provider value={pageProps.product}>
-        <Component {...pageProps} />
-      </ProductContext.Provider>
-    </ProductsContext.Provider>
+    <CartProvider>
+      <ProductsContext.Provider value={pageProps.products}>
+        <ProductContext.Provider value={pageProps.product}>
+          <Component {...pageProps} />
+        </ProductContext.Provider>
+      </ProductsContext.Provider>
+    </CartProvider>
   );
 }
