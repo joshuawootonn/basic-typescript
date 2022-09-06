8a3592587b9f8b49c346657a9d6cafd8c3324cce
Joshua Wootonn <josh@makeswift.com>
2022-09-06 09:49:42 -0500

    feat: add image source

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-const withMakeswift = require('@makeswift/runtime/next/plugin')()
+const withMakeswift = require("@makeswift/runtime/next/plugin")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["cdn.shopify.com"],
+  },
+};
 
-module.exports = withMakeswift(nextConfig)
+module.exports = withMakeswift(nextConfig);
